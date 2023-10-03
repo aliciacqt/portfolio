1fbf1c6e1d57e0da396e79f9595da2360d25d73d
alicia coquerant <alicia.coquerant@gmail.com>
2023-10-03 19:57:55 +0200

    CV update

--- backend/src/routes/projects.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const projectControllers = require("../controllers/projectControllers");
-
-router.get("/", projectControllers.browse);
-router.get("/:id", projectControllers.read);
-
-module.exports = router;
--- backend/src/routes/tools.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const toolControllers = require("../controllers/toolControllers");
-
-router.get("/", toolControllers.browse);
-router.get("/:id", toolControllers.read);
-
-module.exports = router;
